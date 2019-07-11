390170d7e5cd58d9e19444bf6b17a5b080dcb4ea
op7ic <3172590+op7ic@users.noreply.github.com>
2019-07-11 21:04:08 +0300

    Update manualSysmon.bat

--- tools/manualSysmon.bat
+++ tools/manualSysmon.bat
@@ -2,16 +2,21 @@
 echo [+] Detecting OS processor type
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT
 echo [+] X86 present. Installing X86 Sysmon
+SET SYSMONDIR=C:\windows\sysmon64.exe
+IF Not EXIST %SYSMONDIR% (
 C:\SYSMONx730185\sysmon.exe -n -l -d SYSMC186 -accepteula -i C:\SYSMONx730185\sysmonconfig-export.xml
 sc failure SYSMC186 actions= restart/10000/restart/10000// reset= 120
 sc qc SYSMC186 > SYSService-%computername%.txt
 echo [+] Creating Auto Removal Task Which Will Kill Sysmon After 3 weeks
 SchTasks /Create /RU SYSTEM /RL HIGHEST /SC weekly /mo 3 /TN KILLMON /TR "C:\SYSMONx730185\sysmon.exe -u" /F
 :: Increase logging space
-wevtutil.exe sl Microsoft-Windows-Sysmon/Operational /ms:209715200 
+wevtutil.exe sl Microsoft-Windows-Sysmon/Operational /ms:209715200
+) 
 goto END
 :64BIT
 echo [+] X64 present. Installing X64 Sysmon
+SET SYSMONDIR=C:\windows\sysmon.exe
+IF Not EXIST %SYSMONDIR% (
 C:\SYSMONx730185\sysmon64.exe -n -l -accepteula -d SYSMC164 -i C:\SYSMONx730185\sysmonconfig-export.xml
 sc failure SYSMC164 actions= restart/10000/restart/10000// reset= 120
 sc qc SYSMC164 > SYSService-%computername%.txt
@@ -19,6 +24,7 @@ echo [+] Creating Auto Removal Task Which Will Kill Sysmon After 3 weeks
 SchTasks /Create /RU SYSTEM /RL HIGHEST /SC weekly /mo 3 /TN KILLMON /TR "C:\SYSMONx730185\sysmon64.exe -u" /F
 :: Increase logging space
 wevtutil.exe sl Microsoft-Windows-Sysmon/Operational /ms:209715200 
+)
 :END
 echo [+] Sysmon Successfully Installed!
 exit
